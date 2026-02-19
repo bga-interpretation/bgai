171c4c5b402f6cdcc17674e181772954d17d0245
bga-interpretation <nolz.matthias@gmail.com>
2026-02-19 19:40:17 +0100

    aktualisiert

--- src/pages/Training.js
+++ src/pages/Training.js
@@ -3,10 +3,11 @@ import Layout from '@theme/Layout';
 
 export default function training() {
   return (
-    <Layout title="Start" description="BGA Lernplattform">
+    <Layout title="Training" description="BGA Training">
       <main style={{padding: '2rem'}}>
         <h1>Über mich</h1>
-        <p>Ich bin ich, du bist du, jede Zelle zählt dazu. Und wenn im Körper etwas hakt, wird die BGA gleich ausgepackt.</p>
+        <p>Ich bin ich, du bist du. Jede Zelle gehört dazu. Wenn im Körper etwas hakt, wird die BGA gleich ausgepackt.
+       </p>
       </main>
     </Layout>
   );
